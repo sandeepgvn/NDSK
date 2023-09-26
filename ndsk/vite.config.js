6ab9f0e6132d36e3787eb0c7b924e885ac74f89c
sandeepgvn <sandeep.gvn@gmail.com>
2023-09-26 21:47:03 +0530

    Updated  the files

--- ndsk/vite.config.js
+++ ndsk/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // Other configuration options can be added here:
+    build: {
+        // Configuration for production builds
+        outDir: 'build', // Specify the output directory for production builds
+    },
+    server: {
+        // Configuration for the development server
+        port: 3000, // Specify the port for the development server
+    },
 })
